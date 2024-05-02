98a8856d7ab09cfb1642f199a6331e564b79b6ab
zcjswx <zcjswx@hotmail.com>
2024-05-01 22:47:26 -0400

    *add setup script

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 test:
 	go test -tags=test -v -count=1 ./app/...
 
+setup:
+	docker compose -f compose.yaml up -d
+
 run_query:
 	go build -tags=build -o query ./cmd/query.go
 	chmod +x ./query
--- sql/make_table.sql
+++ sql/make_table.sql
@@ -1,31 +1,40 @@
-create table graph
+create table if not exists graph
 (
-    id       integer not null
+    id       integer generated always as identity
         constraint graph_pk
             primary key,
     name     varchar not null,
     graph_id varchar not null
 );
 
-create table node
+create table if not exists node
 (
-    id           integer not null
+    id           integer generated always as identity
         constraint nodes_pk
             primary key,
     name         varchar not null,
     node_id      varchar not null,
     ref_graph_id integer not null
+        constraint node_graph_id_fk
+            references graph
 );
 
-create table edge
+create table if not exists edge
 (
-    id               integer          not null
+    id               integer generated always as identity
         constraint edge_pk
             primary key,
     edge_id          varchar          not null,
     "from"           varchar          not null,
-    ref_from_node_id integer          not null,
+    ref_from_node_id integer          not null
+        constraint edge_from_node_id_fk
+            references node,
     "to"             varchar          not null,
-    ref_to_node_id   integer          not null,
-    cost             double precision not null
+    ref_to_node_id   integer          not null
+        constraint edge_to_node_id_fk
+            references node,
+    cost             double precision not null,
+    ref_graph_id     integer          not null
+        constraint edge_graph_id_fk
+            references graph
 );
